8f821efb000398053f405147b718a94189eefd14
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-05-05 00:18:51 -0400

    Bot tracks last status to compare with new status to avoid repeats

--- Lil-Buddy/__main__.py
+++ Lil-Buddy/__main__.py
@@ -5,13 +5,22 @@ import json
 import discord
 from discord.ext import commands
 
-dir = os.path.dirname(__file__) + "/"
-watch_statuses_file = f"{dir}watch_statuses.json"
+#Cache
+
+#Filenames
+pwdir = os.path.dirname(__file__) + "/"
+watch_statuses_file = f"{pwdir}watch_statuses.json"
 
 #The bot
 lilbuddy = commands.Bot(command_prefix = 'lb.', intents = discord.Intents().all())
 
 #Utility functions
+def get_watchStatuses(): 
+	with open(watch_statuses_file) as f: return json.load(f)
+	
+def save_watchStatuses(new_watchStatuses): 
+	with open(watch_statuses_file, 'w') as f: json.dump(new_watchStatuses, f, indent = 4) 
+
 def can_singleTrigger(user):
 	"""
 	Get @everyone role with id tracked in watch_statuses as 'singleEventTrigger_id'. 
@@ -21,8 +30,7 @@ def can_singleTrigger(user):
 		user (Member): the person to check if we can singleTrigger.
 	"""
 	
-	with open(watch_statuses_file) as f:
-		watch_statuses = json.load(f)
+	watch_statuses = get_watchStatuses()
 		
 	return next((role for role in user.roles if role.id == watch_statuses[str(user.id)]["singleEventTrigger_id"]), False)
 
@@ -39,22 +47,31 @@ def watchingForStatus(user, oldActivity, newActivity):
 		A channel ID (int) if we are watching the user and their activity has changed.
 		False (bool) if one of the conditions is not met
 	"""
+
+	if not newActivity: return False
 	
-	with open(watch_statuses_file) as f:
-		watch_statuses = json.load(f)
+	watch_statuses = get_watchStatuses()
 		
 	if str(user.id) in watch_statuses.keys():
 		
-		if ((oldActivity != newActivity) and can_singleTrigger(user)): return watch_statuses[str(user.id)]["channel"]
+		print(f"oldActivity: {oldActivity}")
+		print(f"newActivity: {newActivity}")
+		print(f"watch_statuses[str(user.id)]['lastStatus']: {watch_statuses[str(user.id)]['lastStatus']}\n")
+		
+		#If newActivity is different from both oldActivity and the last saved status
+		if ((oldActivity != newActivity) and (watch_statuses[str(user.id)]["lastStatus"] != newActivity)): 
+			
+			watch_statuses[str(user.id)]["lastStatus"] = newActivity
+			save_watchStatuses(watch_statuses)
+			
+			return watch_statuses[str(user.id)]["channel"]
 	
 	return False
 
 #Command functions
 @lilbuddy.event
 async def on_ready():
-	"""
-	Detects when the bot has been fully loaded and is online
-	"""
+	""" Detects when the bot has been fully loaded and is online """
 	
 	print("Bot ready!")
 	
@@ -67,12 +84,16 @@ async def on_member_update(before, after):
 		before (Member): the member's old info
 		after (Member): the member's new info
 	"""
+		
+	#If we can't trigger an event this user object, end function here
+	if not can_singleTrigger(after): return
+	if not (after.activity): return
 	
-	statusChannel_id = watchingForStatus(before, before.activity, after.activity)
+	statusChannel_id = watchingForStatus(before, str(before.activity), str(after.activity))
 	
-	if (statusChannel_id and after.activity):
+	if (statusChannel_id):
 		statusChannel = lilbuddy.get_channel(statusChannel_id)
-		await statusChannel.send(f"<@{after.id}> new status:\n> {after.activity}")
+		await statusChannel.send(f"> {after.activity}")
 
 @lilbuddy.command()
 async def ping(ctx):
