0d49eb920056a0e33a6583b40db859ed52652ebb
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-06-16 14:58:13 -0400

    Made further changes to schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,25 +1,26 @@
+DROP DATABASE LilBuddy;
+
 CREATE DATABASE IF NOT EXISTS LilBuddy DEFAULT CHARACTER SET = 'utf8mb4';
 
 USE LilBuddy;
 
 CREATE TABLE IF NOT EXISTS `Servers` (
-	`discord_id` int(24) NOT NULL,
+	`discord_id` BIGINT UNSIGNED NOT NULL,
 	`created` timestamp NOT NULL DEFAULT current_timestamp(),
 	PRIMARY KEY (`discord_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Users` (
-	`discord_id` int(24) NOT NULL,
+	`discord_id` BIGINT UNSIGNED NOT NULL,
 	`created` timestamp NOT NULL DEFAULT current_timestamp(),
 	PRIMARY KEY (`discord_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `WatchStatuses` (
-	`id` int(10) NOT NULL,
-	`user_id` int(24) NOT NULL,
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`user_id` BIGINT UNSIGNED NOT NULL,
 	`status` VARCHAR(129),
-	`status_channel` int(24) NOT NULL,
+	`status_channel` BIGINT UNSIGNED NOT NULL,
 	`created` timestamp NOT NULL DEFAULT current_timestamp(),
-	PRIMARY KEY (`user_id`),
-	CONSTRAINT `WatchStatuses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`discord_id`)
-);
+	PRIMARY KEY (`id`)
+); 
